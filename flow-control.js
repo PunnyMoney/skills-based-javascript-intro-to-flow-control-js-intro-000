2161a47e0da13d29c925a7a795267dbce008819c
Penelope Jungreis <penelope.jungreis@gmail.com>
2020-06-01 16:27:39 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,41 @@
 function basicTeenager(age) {
-
+if (age > 12 && age < 20) {
+  return 'You are a teenager!';
 }
+}
+teenager(16);
 
 function teenager(age) {
-
+if (age > 12 && age < 20) {
+  return 'You are a teenager!';
+} else {
+  return 'You are not a teenager';
 }
+}
+teenager(15);
 
 function ageChecker(age) {
-
+if (age > 12 && age < 20) {
+  return 'You are a teenager!';
+} else if (age <= 12){
+  return 'You are a kid';
+} else if (age > 19) {
+  return 'You are a grownup';
+}
 }
 
 function ternaryTeenager(age) {
-
+return age > 12 && age < 20 ? 'You are a teenager' : 'You are not a teenager';
 }
 
+var age = 15;
 function switchAge(age) {
-
+  switch(age) {
+  case age > 12 && age < 20:
+  console.log('You are a teenager');
+  break;
+  default:
+  console.log('You have an age');
+  break;
+}
 }
